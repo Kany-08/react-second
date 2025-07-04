2fc08e7ac396ccdca71d5eb1802d531630d0e521
kany <kanysharapova@gmail.com>
2025-07-04 12:08:22 +0200

    refactor: css button modules

---
+++ src/components/ThemeSwitchButton/ThemeSwitchButton.jsx
@@ -0,0 +1,11 @@
+import S from "./ThemeSwitchButton.module.css";
+
+export const ThemeSwitchButton = ({ value, active, onSwitch, children }) => {
+  const className = active ? `${S.container} ${S.active}` : S.container;
+
+  return (
+    <button onClick={() => onSwitch(value)} className={className}>
+      {children}
+    </button>
+  );
+};
