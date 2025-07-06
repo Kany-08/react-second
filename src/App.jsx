ad0b090f9844657f4a451e2bcc0f4f4786b621bb
kany <kanysharapova@gmail.com>
2025-07-06 14:26:40 +0200

    refactor: final result

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import './App.css'
-import { ThemeSwitch } from './components/ThemeSwitch/ThemeSwitch'
+import { useState } from "react";
+import "./App.css";
+import { ThemeSwitch } from "./components/ThemeSwitch/ThemeSwitch";
+import themeButtons from "./config/theme-buttons.json";
+import classNames from "classnames";
 
 function App() {
-
-  return <ThemeSwitch />
+  const [theme, setTheme] = useState("light");
+  const onThemeSwitch = (theme) => {
+    console.log(theme);
+    setTheme(theme);
+  };
+  return (
+    <div className={classNames(theme, "app")}>
+      <ThemeSwitch
+        buttons={themeButtons}
+        theme={theme}
+        onThemeSwitch={onThemeSwitch}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
